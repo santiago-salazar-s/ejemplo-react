f7e00dcb616a9effb66c379e2215dd0bb55ad755
santiago-salazar-s <santiagos9612@gmail.com>
2023-08-01 11:04:35 -0500

    cambie mi nombre

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          
+          <code className={styles.code}>Santiago  app/page.js</code>
         </p>
         <div>
           <a
